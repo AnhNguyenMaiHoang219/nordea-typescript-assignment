68b20867286d42219b6573750a0134ba9408b5b0
Anh Nguyen <nguyenhoanganh219@gmail.com>
2023-12-13 04:06:59 +0200

    Shorten the input band object

--- src/main.ts
+++ src/main.ts
@@ -4,43 +4,15 @@ import { union } from './util';
 const inputBand: Band = {
   members: {
     current: [
-      {
-        name: 'Sascha',
-        age: 59,
-        plays: ['vocals', 'synth', 'guitar', 'bass'],
-      },
-      {
-        name: 'Lucia',
-        age: 49,
-        plays: ['vocals', 'synth'],
-      },
-      {
-        name: 'Jules',
-        age: 53,
-        plays: ['guitar', 'bass', 'synth'],
-      },
-      {
-        name: 'Steve',
-        age: 55,
-        plays: ['guitar'],
-      },
+      { name: 'Sascha', age: 59, plays: ['vocals', 'synth', 'guitar', 'bass'] },
+      { name: 'Lucia', age: 49, plays: ['vocals', 'synth'] },
+      { name: 'Jules', age: 53, plays: ['guitar', 'bass', 'synth'] },
+      { name: 'Steve', age: 55, plays: ['guitar'] },
     ],
     past: [
-      {
-        name: 'Raymond',
-        age: 57,
-        plays: ['vocals', 'synth'],
-      },
-      {
-        name: 'En',
-        age: 52,
-        plays: ['vocals', 'drums', 'guitar', 'synth'],
-      },
-      {
-        name: 'Gunter',
-        age: 57,
-        plays: ['guitar', 'synth'],
-      },
+      { name: 'Raymond', age: 57, plays: ['vocals', 'synth'] },
+      { name: 'En', age: 52, plays: ['vocals', 'drums', 'guitar', 'synth'] },
+      { name: 'Gunter', age: 57, plays: ['guitar', 'synth'] },
     ],
   },
 };
